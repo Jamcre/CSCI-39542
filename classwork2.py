1d118b8201f09378c66351b4f59b558f13682e5f
Shhmames <101753462+Shhmames@users.noreply.github.com>
2023-02-01 12:53:21 -0500

    1st attempt

--- classwork2.py
+++ classwork2.py
@@ -6,3 +6,18 @@
     Row: 4
     Seat: 42
 """
+
+import pandas as pd
+
+# Ask user for input and output file names
+inFile = input("Enter input file name: ")
+outFile = input("Enter output file name: ")
+
+# Open and read file into dataframe
+df = pd.read_csv(inFile)
+
+# Select rows where 'Grade' = 3 and 'Year' = 2019, and write to new csv
+out_df = df[(df['Grade'] == 3) and (df['Year'] == 2019)]
+
+# .to_csv(outFile)
+out_df.to_csv((outFile))
