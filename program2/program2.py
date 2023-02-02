8ea6dd2b61824b12aef5094d500e69b020b87e3b
Shhmames <101753462+Shhmames@users.noreply.github.com>
2023-02-01 21:43:24 -0500

    added stubs

---
+++ program2/program2.py
@@ -0,0 +1,27 @@
+"""
+Name:  James Crespo
+Email: james.crespo64@myhunter.cuny.edu
+Resources: the internet
+"""
+
+
+def clean_df(df, year=2015):
+    return df
+
+
+def filter_health(df, keep):
+    return df
+
+
+def add_indicator(row):
+    return 0
+
+
+def find_trees(df, species):
+    lst = []
+    return lst
+
+
+def count_by_area(df, area='boroname'):
+    total = 0
+    return total
