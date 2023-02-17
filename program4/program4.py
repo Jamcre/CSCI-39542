e2d7f13167903bf24586aa1fededf5cc3f012c36
Shhmames <101753462+Shhmames@users.noreply.github.com>
2023-02-16 22:31:24 -0500

    checking 1st function

--- program4/program4.py
+++ program4/program4.py
@@ -9,7 +9,17 @@ import matplotlib.pyplot as plt
 
 
 def make_dog_df(license_file, zipcode_file):
-    return df
+    """
+
+    """
+    license_df = pd.read_csv(license_file)
+    zipcode_df = pd.read_csv(zipcode_file)
+    zipcode_df = zipcode_df.rename(column={'zip': 'ZipCode'})
+    merged_df = pd.merge(license_df, zipcode_df, how='left', on='ZipCode')
+    merged_df = merged_df.drop(['LicenseExpiredDate', 'Extract Year'])
+    merged_df['AnimalName'] = merged_df['AnimalName'].str.capitalize()
+    merged_df = merged_df.loc[(df['borough'] != 'NaN')]
+    return merged_df
 
 
 def make_bite_df(file_name):
@@ -40,58 +50,56 @@ def main():
     """
     function tests
     """
-    # make_dog_df() test
     dog_df = make_dog_df("NYC_Dog_Licensing_Dataset_2021.csv",
                          "nyc_zip_borough_neighborhoods_pop.csv")
     print(dog_df)
 
-    # parse_datetime() test
-    dog_df = parse_datetime(dog_df)
-    print(dog_df)
+    # dog_df = parse_datetime(dog_df)
+    # print(dog_df)
 
-    print('Most popular names are:')
-    print(dog_df['AnimalName'].value_counts()[:10])
+    # print('Most popular names are:')
+    # print(dog_df['AnimalName'].value_counts()[:10])
 
-    sns.histplot(data=dog_df, x="Borough")
+    # sns.histplot(data=dog_df, x="Borough")
 
-    plt.title('Dog Bites, 2021')
-    plt.show()
+    # plt.title('Dog Bites, 2021')
+    # plt.show()
 
-    bite_df = make_bite_df("DOHMH_Dog_Bite_Data_2021.csv")
-    print(bite_df)
+    # bite_df = make_bite_df("DOHMH_Dog_Bite_Data_2021.csv")
+    # print(bite_df)
 
-    sns.histplot(data=bite_df, x="Borough")
-    plt.show()
+    # sns.histplot(data=bite_df, x="Borough")
+    # plt.show()
 
-    df_drop = bite_df.dropna()
-    print(f'The full DataFrame has {len(bite_df)} entries.')
-    print(f'Dropping undefined values leaves {len(df_drop)} entries.')
+    # df_drop = bite_df.dropna()
+    # print(f'The full DataFrame has {len(bite_df)} entries.')
+    # print(f'Dropping undefined values leaves {len(df_drop)} entries.')
 
-    bite_df['Age Num'] = bite_df['Age'].apply(clean_age)
-    print(bite_df[['Age', 'Age Num']])
+    # bite_df['Age Num'] = bite_df['Age'].apply(clean_age)
+    # print(bite_df[['Age', 'Age Num']])
 
-    bite_df['Breed'] = bite_df['Breed'].apply(clean_breed)
-    print(bite_df)
+    # bite_df['Breed'] = bite_df['Breed'].apply(clean_breed)
+    # print(bite_df)
 
-    bite_df = impute_age(bite_df)
-    bite_df['ZipCode'] = bite_df.apply(
-        lambda row: impute_zip(row['Borough'], row['ZipCode']), axis=1)
-    print(bite_df)
+    # bite_df = impute_age(bite_df)
+    # bite_df['ZipCode'] = bite_df.apply(
+    #     lambda row: impute_zip(row['Borough'], row['ZipCode']), axis=1)
+    # print(bite_df)
 
-    bite_df = parse_datetime(bite_df, column='DateOfBite')
-    print(bite_df)
+    # bite_df = parse_datetime(bite_df, column='DateOfBite')
+    # print(bite_df)
 
-    sns.histplot(data=bite_df, x="month", discrete=True)
+    # sns.histplot(data=bite_df, x="month", discrete=True)
 
-    plt.xticks(range(1, 13),
-               ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'])
-    plt.title('Dog Bites, 2021')
-    plt.show()
+    # plt.xticks(range(1, 13),
+    #            ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'])
+    # plt.title('Dog Bites, 2021')
+    # plt.show()
 
-    sns.histplot(data=bite_df, x="day_of_week")
-    plt.xticks(range(7), ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'])
-    plt.title('Dog Bites, 2021')
-    plt.show()
+    # sns.histplot(data=bite_df, x="day_of_week")
+    # plt.xticks(range(7), ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'])
+    # plt.title('Dog Bites, 2021')
+    # plt.show()
 
 
 if __name__ == "__main__":
