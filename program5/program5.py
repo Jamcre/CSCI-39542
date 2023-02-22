4f0a5b8a8b1954ab63706333a70e882102dabe9a
Shhmames <101753462+Shhmames@users.noreply.github.com>
2023-02-22 11:45:35 -0500

    stubs for program 5

---
+++ program5/program5.py
@@ -0,0 +1,44 @@
+"""
+    Name: James Michael Crespo
+    Email: james.crespo64@myhunter.cuny.edu
+    Resources:  Internet
+"""
+
+
+def parse_datetime(df, column='DATE'):
+    return df
+
+
+def compute_lin_reg(xes, yes):
+    return 0, 1
+
+
+def predict(xes, theta_0, theta_1):
+    return 0, 1
+
+
+def mse_loss(y_actual, y_estimate):
+    return 0
+
+
+def rmse_loss(y_actual, y_estimate):
+    return 0
+
+
+def compute_error(y_actual, y_estimate, loss_fnc=mse_loss):
+    return 0
+
+
+def compute_ytd(df):
+    return []
+
+
+def compute_year_over_year(df):
+    return []
+
+
+def main():
+
+
+if __name__ == "__main__":
+    main()
