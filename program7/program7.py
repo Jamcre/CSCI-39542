d1672882067d592fecaca02fb9eb8e25399f91f0
Shhmames <101753462+Shhmames@users.noreply.github.com>
2023-03-08 12:11:56 -0500

    created folder

---
+++ program7/program7.py
@@ -0,0 +1,13 @@
+"""
+Name:  James Michael Crespo
+Email: james.crespo64@myhunter.cuny.edu
+Resources: the internet
+"""
+
+
+def main():
+    """tests"""
+
+
+if __name__ == "__main__":
+    main()
