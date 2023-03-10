ae810bcca7efd85c13297a166fc1b9cbc7b89a29
Shhmames <101753462+Shhmames@users.noreply.github.com>
2023-03-10 02:10:34 -0500

    check fit_poly

--- program7/program7.py
+++ program7/program7.py
@@ -5,7 +5,6 @@ Resources: the internet
 """
 import pickle
 import pandas as pd
-import numpy as np
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.linear_model import LinearRegression, LassoCV, RidgeCV
 from sklearn.model_selection import train_test_split
@@ -57,34 +56,16 @@ def fit_poly(xes, yes, epsilon=100, verbose=False):
     (in format: f'MSE cost for deg {deg} poly model: {error:.3f}' for degrees 1, 2, ...,
     until the error is below epsilon, see example below). Default=False.
     """
-    def fit_poly(xes, yes, epsilon=100, verbose=False):
-        # Initialize polynomial degree and error
-        deg = 1
-        error = np.inf
-
-        # Continue increasing polynomial degree until error is below epsilon
-        while error > epsilon:
-            # Create polynomial features
-            poly = PolynomialFeatures(degree=deg)
-            x_poly = poly.fit_transform(xes)
-
-            # Fit linear regression model
-            model = LinearRegression()
-            model.fit(x_poly, yes)
-
-            # Compute mean squared error on training set
-            y_pred = model.predict(x_poly)
-            error = mean_squared_error(yes, y_pred)
-
-        # Print error if verbose is True
+    # Iterate from deg=1 to deg=5
+    for deg in range(1, 6):
+        x_poly = PolynomialFeatures(deg).fit_transform(xes)
+        model = LinearRegression(fit_intercept=False).fit(x_poly, yes)
+        error = mean_squared_error(yes, model.predict(x_poly))
         if verbose:
             print(f'MSE cost for deg {deg} poly model: {error:.3f}')
-
-        # Increase degree for next iteration
-        deg += 1
-
-    # Return the last model fitted (i.e., the one with error below epsilon)
-    return model
+        if error < epsilon:
+            return deg
+    return None
 
 
 def fit_model(xes, yes, poly_deg=2, reg="lasso"):
